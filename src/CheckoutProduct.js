930d41f54c897d265c8ab96e77ed4854e048747e
rounit08 <rounitrsinha@gmail.com>
2022-01-29 12:01:59 +0530

    first commit

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -3,7 +3,7 @@ import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price }) {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
 
   const removeFromBasket = () => {
     dispatch({
@@ -13,7 +13,7 @@ function CheckoutProduct({ id, image, title, price }) {
   };
   return (
     <div className="checkoutProduct">
-      <img className="checkoutProduct__image" src={image} />
+      <img className="checkoutProduct__image" src={image} alt="" />
 
       <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{title}</p>
