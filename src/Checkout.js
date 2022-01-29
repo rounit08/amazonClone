83ad387c8f18362e6d635bec46221a7884e227c6
rounit08 <rounitrsinha@gmail.com>
2022-01-29 12:02:12 +0530

    ok

--- src/Checkout.js
+++ src/Checkout.js
@@ -12,6 +12,7 @@ function Checkout() {
         <img
           className="checkout__ad"
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
         />
 
         <div>
